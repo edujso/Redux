6e5b57cbbfc668c0371cdbc7790e5a1511425878
edin.dujso <edin.dujso@API1086>
2017-04-07 18:26:59 +0200

    Redux part 1

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from './course.service';
 import { Course } from './course';
-import { FilterTextComponent, FilterService } from '../blocks/filter-text';
+//import { FilterTextComponent, FilterService } from '../blocks/filter-text';
+import { FilterTextComponent } from '../blocks/filter-text';
+import { store, filterCourses, IAppState } from '../store';
+import { NgRedux, select } from 'ng2-redux';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-course-list',
@@ -9,26 +14,31 @@ import { FilterTextComponent, FilterService } from '../blocks/filter-text';
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[];
-  filteredCourses = this.courses;
+  @select('filteredCourses') filteredCourses$: Observable<Course>
+  //courses: Course[];
+  //filteredCourses = this.courses;
+  filteredCourses = [];
 
-  constructor(private _courseService: CourseService, private _filterService: FilterService) {
-  }
+/*  constructor(private _courseService: CourseService, private _filterService: FilterService) {
+  }*/
+    constructor(
+      private NgRedux: NgRedux<IAppState>
+      ) { }
 
   filterChanged(searchText: string) {
     console.log('user searched: ', searchText);
-    this.filteredCourses = this._filterService.filter(searchText, ['id', 'name', 'topic'], this.courses);
+    //this.filteredCourses = this._filterService.filter(searchText, ['id', 'name', 'topic'], this.courses);
+    store.dispatch(filterCourses(searchText));
   }
 
-  getCourses() {
+/*  getCourses() {
     this._courseService.getCourses()
       .subscribe(courses => {
         this.courses = this.filteredCourses = courses;
       });
-  }
+  }*/
 
   ngOnInit() {
-    this.getCourses();
     componentHandler.upgradeDom();
   }
 }
---
+++ src/app/store/reducer.ts
@@ -0,0 +1,41 @@
+import { Course } from '../courses/course';
+import { IAppState } from './IAppState';
+import { FILTER_COURSES } from './actions'; 
+
+const courses = [
+      {
+        "id": 1,
+        "name": "Building Apps with React",
+        "topic": "ReactJS"
+      },
+      {
+        "id": 2,
+        "name": "Building Apps with Angular",
+        "topic": "AngularJS"
+      },
+      {
+        "id":3,
+        "name": "Building Apps with Angular and Redux",
+        "topic": "Angular and Redux"
+      }
+    ];
+
+const initialState: IAppState = {
+    courses,
+    filteredCourses: courses
+}
+
+function filterCourses(state, action) : IAppState {
+    return Object.assign({}, state, {
+        filteredCourses: state.courses.filter( c => c.name.toLowerCase().indexOf(action.searchText.toLowerCase()) > -1),
+    })
+}
+
+export function reducer( state = initialState, action) {
+    switch(action.type) {
+        case FILTER_COURSES:
+            return filterCourses(state, action);
+        default:
+            return state;
+    }
+}
